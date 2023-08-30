0688c7365e31f0c263286d1e1812861f74d3f5c2
technicca <cake3327@gmail.com>
2023-08-30 23:12:55 +0300

    update

--- c_xor.c
+++ c_xor.c
@@ -2,15 +2,21 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define INPUT_NEURONS 2
+#define HIDDEN_NEURONS 19
+#define OUTPUT_NEURONS 1
+#define LEARNING_RATE 0.8
+#define EPOCHS 20000
+
 // Define the structure of a neuron
 typedef struct {
-    double weights[2];
+    double weights[INPUT_NEURONS];
     double bias;
 } Neuron;
 
 // Define the structure of a network
 typedef struct {
-    Neuron hidden[2];
+    Neuron hidden[HIDDEN_NEURONS];
     Neuron output;
 } Network;
 
@@ -25,19 +31,19 @@ double sigmoid_prime(double x) {
 }
 
 // Define the feedforward function
-double feedforward(Network* network, double inputs[2]) {
-    double hidden_outputs[2];
-    for (int i = 0; i < 2; i++) {
+double feedforward(Network* network, double inputs[INPUT_NEURONS]) {
+    double hidden_outputs[HIDDEN_NEURONS];
+    for (int i = 0; i < HIDDEN_NEURONS; i++) {
         hidden_outputs[i] = sigmoid(network->hidden[i].weights[0] * inputs[0] + network->hidden[i].weights[1] * inputs[1] + network->hidden[i].bias);
     }
     return sigmoid(network->output.weights[0] * hidden_outputs[0] + network->output.weights[1] * hidden_outputs[1] + network->output.bias);
 }
 
 // Define the train function
-void train(Network* network, double inputs[2], double target) {
+void train(Network* network, double inputs[INPUT_NEURONS], double target) {
     // Feedforward
-    double hidden_outputs[2];
-    for (int i = 0; i < 2; i++) {
+    double hidden_outputs[HIDDEN_NEURONS];
+    for (int i = 0; i < HIDDEN_NEURONS; i++) {
         hidden_outputs[i] = sigmoid(network->hidden[i].weights[0] * inputs[0] + network->hidden[i].weights[1] * inputs[1] + network->hidden[i].bias);
     }
     double output = sigmoid(network->output.weights[0] * hidden_outputs[0] + network->output.weights[1] * hidden_outputs[1] + network->output.bias);
@@ -49,24 +55,24 @@ void train(Network* network, double inputs[2], double target) {
     double output_gradient = output_error * sigmoid_prime(output);
 
     // Adjust output weights and bias
-    for (int i = 0; i < 2; i++) {
-        network->output.weights[i] += output_gradient * hidden_outputs[i];
+    for (int i = 0; i < HIDDEN_NEURONS; i++) {
+        network->output.weights[i] += output_gradient * hidden_outputs[i] * LEARNING_RATE;
     }
-    network->output.bias += output_gradient;
+    network->output.bias += output_gradient * LEARNING_RATE;
 
     // Calculate hidden errors
-    double hidden_errors[2];
-    for (int i = 0; i < 2; i++) {
+    double hidden_errors[HIDDEN_NEURONS];
+    for (int i = 0; i < HIDDEN_NEURONS; i++) {
         hidden_errors[i] = network->output.weights[i] * output_error;
     }
 
     // Calculate hidden gradients and adjust hidden weights and biases
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < HIDDEN_NEURONS; i++) {
         double hidden_gradient = hidden_errors[i] * sigmoid_prime(hidden_outputs[i]);
-        for (int j = 0; j < 2; j++) {
-            network->hidden[i].weights[j] += hidden_gradient * inputs[j];
+        for (int j = 0; j < INPUT_NEURONS; j++) {
+            network->hidden[i].weights[j] += hidden_gradient * inputs[j] * LEARNING_RATE;
         }
-        network->hidden[i].bias += hidden_gradient;
+        network->hidden[i].bias += hidden_gradient * LEARNING_RATE;
     }
 }
 
@@ -75,17 +81,30 @@ int main() {
     Network network = {0};
 
     // Define training data
-    double inputs[4][2] = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+    double inputs[4][INPUT_NEURONS] = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
     double targets[4] = {0, 1, 1, 0};
 
     // Train network
-    for (int i = 0; i < 10000; i++) {
+    for (int i = 0; i < EPOCHS; i++) {
         for (int j = 0; j < 4; j++) {
             train(&network, inputs[j], targets[j]);
         }
     }
 
+    // Print final weights and biases
+    printf("Final hidden weights:\n");
+    for (int i = 0; i < HIDDEN_NEURONS; i++) {
+        printf("Neuron %d: %f, %f\n", i, network.hidden[i].weights[0], network.hidden[i].weights[1]);
+    }
+    printf("Final hidden biases:\n");
+    for (int i = 0; i < HIDDEN_NEURONS; i++) {
+        printf("Neuron %d: %f\n", i, network.hidden[i].bias);
+    }
+    printf("Final output weights: %f, %f\n", network.output.weights[0], network.output.weights[1]);
+    printf("Final output bias: %f\n", network.output.bias);
+
     // Test network
+    printf("\nOutput from neural network after %d epochs:\n", EPOCHS);
     for (int i = 0; i < 4; i++) {
         printf("XOR(%d, %d) = %f\n", (int)inputs[i][0], (int)inputs[i][1], feedforward(&network, inputs[i]));
     }
